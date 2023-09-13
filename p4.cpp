44da50625fdd0b41e263e81609198e4192a5184b
bhavyb <144156101+bhavyb@users.noreply.github.com>
2023-09-13 17:21:07 +0530

    Add files via upload

---
+++ p4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class student
+{
+    private:
+        string name;
+        int age;
+
+    public:
+        void get_details()
+        {
+            cout<<"Enter your good name :: ";
+            getline(cin,name);
+            cout<<"Enter your age :: ";
+            cin>>age;
+        }
+         void display_details()
+        {
+            cout<<"\nName is :: " <<name <<endl;
+            cout<<"Age is :: "<< age<<" years"<<endl;
+        }
+};
+int main()
+{
+    student s1;
+    s1.get_details();
+    s1.display_details();
+    return 0;
+    
+}
